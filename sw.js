87ca84065adf6425c1bd3a64a27eb18b95a49c20
Ramonb-Tools <ramonb@stanford.edu>
2025-09-18 15:02:38 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,7 @@
-const CACHE_NAME = 'ocr-camera-v1';
-const BASE = '/Mail-OCR2/';
-const ASSETS = [
-  BASE,
-  BASE + 'index.html',
-  BASE + 'manifest.webmanifest',
-  BASE + 'sw.js',
-  'https://cdn.jsdelivr.net/npm/tesseract.js@5/dist/tesseract.min.js'
-];
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+const CACHE_NAME = 'ocr-v2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest'])));
 });
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-  );
-});
-self.addEventListener('fetch', (e) => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin && url.pathname.startsWith(BASE)) {
-    e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
-  } else {
-    e.respondWith(
-      fetch(e.request).then((res) => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then((c) => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match(e.request))
-    );
-  }
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
