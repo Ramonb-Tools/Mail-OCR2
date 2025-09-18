00a5865844e345d68147d20fb9d94d8a29cd3e90
Ramonb-Tools <ramonb@stanford.edu>
2025-09-18 15:24:03 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ocr-camera-v2';
+const CACHE_NAME = 'ocr-camera-v2-1a';
 const BASE = '/Mail-OCR2/';
 const ASSETS = [
   BASE,
@@ -20,12 +20,6 @@ self.addEventListener('fetch', (e) => {
   if (url.origin === location.origin && url.pathname.startsWith(BASE)) {
     e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
   } else {
-    e.respondWith(
-      fetch(e.request).then((res) => {
-        const clone = res.clone();
-        caches.open(CACHE_NAME).then((c) => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match(e.request))
-    );
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
   }
 });
